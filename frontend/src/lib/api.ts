81d71c6798af475cf6438e34815fde84de7acbe8
theconsigliere <hello@maxwellkirwin.co.uk>
2025-05-09 17:46:05 +0100

    1:13:42 tanstack router

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,5 @@
 import { hc } from 'hono/client'
-import { type ApiRoutes } from '../../../server/app';
+import { type ApiRoutes } from "../../../server/app";
 
 // using hono rpc client to fetch the total spent completely type safe from backend to frontend
 const client = hc<ApiRoutes>('/')
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,13 @@
 import path from "path"
 import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), TanStackRouterVite()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
